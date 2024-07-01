654a7daec95185c82cbde3b34ee93d6ecd85a904
nagu014 <23203014@srcas.ac.in>
2024-07-01 11:58:59 +0530

    Create sample.java

---
+++ sample.java
@@ -0,0 +1,7 @@
+public class Hello{
+public static void main (String[] args){
+System.out.println("welcome");
+System.out.println("to");
+System.out.println("Devops");
+System.out.println("class");
+}
